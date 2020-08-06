2e126e37367cfbbdcadee82a38ce84292ed2a1f2
chuliangying <283413016@qq.com>
2020-08-07 00:57:11 +0800

    创建springboot演示项目

---
+++ src/main/java/cn/husky/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package cn.husky.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Success!!");
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/husky/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.husky.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
