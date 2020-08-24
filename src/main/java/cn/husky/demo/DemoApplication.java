28eb3a0cfc552f4462bb27e97e39405dad4d0301
chuliangying <283413016@qq.com>
2020-08-24 22:15:00 +0800

    springboot搭建

--- src/main/java/cn/husky/demo/DemoApplication.java
+++ src/main/java/cn/husky/demo/DemoApplication.java
@@ -1,15 +1,27 @@
 package cn.husky.demo;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class DemoApplication {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class DemoApplication extends SpringBootServletInitializer implements ApplicationRunner {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
 
     public static void main(String[] args) {
-        System.out.println("Success!!");
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("This is a message from Runner");
+    }
 }
--- src/main/java/cn/husky/demo/controller/TestController.java
+++ src/main/java/cn/husky/demo/controller/TestController.java
@@ -1,12 +1,17 @@
 package cn.husky.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${spring.application.name}")
+    private String name;
+
     @RequestMapping("/")
     public String hello(){
-        return "Hello,this is a demo";
+        return "project name:" + name;
     }
 }
